ca7da21b3cabcb410f2e524f0694f05a5644b7ba
lembarek <lembarek2011@gmail.com>
2016-02-22 17:12:04 +0100

    install codeception and codesniffer

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -7,6 +7,9 @@ $baseDir = dirname($vendorDir);
 
 return array(
     '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
+    'a0edc8309cc5e1d60e3047b5df6b7052' => $vendorDir . '/guzzlehttp/psr7/src/functions_include.php',
+    'c964ee0ededf28c96ebd9db5099ef910' => $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
+    '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
     '72579e7bd17821bb1321b87411366eae' => $vendorDir . '/illuminate/support/helpers.php',
     '818eb2d76259e8542f7d92f04646f656' => $baseDir . '/src/helpers.php',
 );
