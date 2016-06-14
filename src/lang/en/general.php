8fde898da9dfcabd34543733fbf773bef635b84a
lembarek <lembarek2011@gmail.com>
2016-06-14 22:01:49 +0100

    new words in lang/en/general.php

--- src/lang/en/general.php
+++ src/lang/en/general.php
@@ -2,4 +2,8 @@
 return [
     'delete' => 'delete',
     'add' => 'add',
+    'username' => 'username',
+    'password' => 'Password',
+    'password_confirmation' => "Password Confirmation",
+    'email' => 'Email',
 ];
