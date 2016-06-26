d23da3e5142b334fb6aab1ac188bbb5a43ce06b2
lembarek <lembarek2011@gmail.com>
2016-06-26 06:11:56 +0100

    use namespace in group in routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,10 @@
 <?php
 
-Route::group(['as' => 'core::', 'middleware' => ['web']], function () {
+Route::group(['namespace ' => 'Lembarek\Core\Controllers', 'as' => 'core::', 'middleware' => ['web']], function () {
 
     Route::get('/', [
     'as' => 'home',
     'uses' => 'Lembarek\Core\Controllers\HomeController@home',
     ]);
+
 });
