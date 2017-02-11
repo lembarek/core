71a7b00b487788295332bd6b47a258ebdfd2c1cd
lembarek <lembarek2011@gmail.com>
2017-02-11 18:06:21 +0100

    add csrf-token to header

--- src/views/layout/partials/header.blade.php
+++ src/views/layout/partials/header.blade.php
@@ -1,4 +1,5 @@
 <meta charset="utf-8">
 <title>@yield('title')</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css" >
