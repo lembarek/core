179ed7e7856a7ecd8a4633872ad6de17f8d8a3ad
lembarek <lembarek2011@gmail.com>
2017-03-02 17:04:30 +0100

    add ufactory function

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,6 @@
 <?php
 
-$files = ['others', 'git', 'string', 'files', 'filesystem', 'db'];
+$files = ['others', 'git', 'string', 'files', 'filesystem', 'db', 'factory'];
 
 foreach($files as $file){
     include_once(__DIR__.'/helpers/'.$file.'.php');
